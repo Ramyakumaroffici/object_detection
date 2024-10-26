f7157b5c980c87d44cb191562280bf13801300e7
Ramyakumaroffici <ramyakumaroffici@gmail.com>
2024-10-26 11:54:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def upload():
 if __name__ == '__main__':
     # Ensure the frames directory exists
     os.makedirs(os.path.join("static", "frames"), exist_ok=True)
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
